fe9cefee696009da2abc9d83bb96286af46a5369
Joshua Morales <joshuapmorales@gmail.com>
2020-12-24 02:20:11 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,16 @@
+
 def square_array(array)
-  # your code here
+  
+  counter = 0 
+
+  new_array = []
+  
+  while array[counter]
+     new_array.push(array[counter] ** 2)
+     counter += 1 
+  end     
+  
+  return new_array
+  
 end
+
